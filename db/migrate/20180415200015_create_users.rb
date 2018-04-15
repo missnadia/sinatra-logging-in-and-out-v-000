42a8691c67e8086f57881fae766217c064861416
Nadia Lee <miss.nadia.lee@gmail.com>
2018-04-15 20:01:22 +0000

    automatically backed up by learn

--- db/migrate/20180415200015_create_users.rb
+++ db/migrate/20180415200015_create_users.rb
@@ -1,4 +1,9 @@
 class CreateUsers < ActiveRecord::Migration[5.1]
   def change
+    create_table :users do |t|
+      t.real :username
+      t.real :password
+      t.float :balance
+    end
   end
 end
